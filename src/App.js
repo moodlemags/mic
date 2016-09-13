a5b0089172d0c99559dd97c70d2a7d8de65b5580
Maggie Merrell <maggiemerrell31@gmail.com>
2016-09-13 13:20:20 -0400

    header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styling/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
+        <header className="global-header">
+            <a className="header-section unpub-articles">UNPUBLISHED ARTICLES(interpolate #)</a>
+            <a className="header-section author">AUTHOR</a>
+            <a className="header-section words">WORDS</a>
+            <a className="header-section submitted">SUBMITTED</a>
+        </header>
+
+        <section className="render-articles">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </section>
       </div>
     );
   }
